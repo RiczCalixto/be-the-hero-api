10b4d98bb8043be5cc2309aef32669b2cb8937b6
Ricardo Calixto <rcalixtos@gmail.com>
2020-03-29 20:51:02 -0300

    Add ong controllers - create and list

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,9 @@
-const express = require('express');
+const express = require("express");
+const OngController = require("./controllers/OngController");
 
 const routes = express.Router();
 
-routes.get("/users", (req, res) => {
-  return res.json({
-    evento: "Semana omniStack",
-    aluno: "RiCc"
-  });
-});
+routes.get("/ongs", OngController.list);
+routes.post("/ongs", OngController.create);
 
 module.exports = routes;
