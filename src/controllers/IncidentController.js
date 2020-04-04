282e5b91fe2d1da90499bd5803156fea1fc4dc30
Ricardo Calixto <rcalixtos@gmail.com>
2020-04-04 14:07:21 -0300

    Add validator

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -40,22 +40,25 @@ module.exports = {
   },
 
   async delete(req, res) {
-    const { id } = req.params;
-    const ong_id = req.headers.authorization;
+    try {
+      const { id } = req.params;
+      const ong_id = req.headers.authorization;
 
-    const incident = await connection("incidents")
-      .where("id", id)
-      .select("ong_id")
-      .first();
+      const incident = await connection("incidents")
+        .where("id", id)
+        .select("ong_id")
+        .first();
+      if (incident.ong_id !== ong_id) {
+        return res.status(401).json({ error: "Operation not permitted." });
+      }
 
-    if (incident.ong_id !== ong_id) {
-      return res.status(401).json({ error: "Operation not permitted." });
+      await connection("incidents")
+        .where("id", id)
+        .delete();
+
+      return res.status(204).send();
+    } catch (error) {
+      return res.status(404).json({ error: "Incident id doesnt not exist." });
     }
-
-    await connection("incidents")
-      .where("id", id)
-      .delete();
-
-    return res.status(204).send();
   }
 };
